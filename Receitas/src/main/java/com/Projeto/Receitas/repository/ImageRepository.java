dd065ae7032cf87201404cf05b1e1965db8d7833
Lucas da Silva <160244553+Lsdceo@users.noreply.github.com>
2024-11-27 11:21:11 -0300

    Delete Receitas directory

--- Receitas/src/main/java/com/Projeto/Receitas/repository/ImageRepository.java
+++
@@ -1,6 +0,0 @@
-package com.Projeto.Receitas.repository;
-import com.Projeto.Receitas.model.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, Long> {
-}
